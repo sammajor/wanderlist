e49c3e446688a042a3fff64753fe04db9fb23231
Sam Major <sam.m.major@gmail.com>
2023-05-22 15:37:42 -0500

    parks router re-added

--- api/main.py
+++ api/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from authenticator import authenticator
-from routers import accounts, trips, trip_notes
+from routers import accounts, trips, trip_notes, parks
 
 app = FastAPI()
 app.include_router(authenticator.router, tags=["Authentication"])
 app.include_router(accounts.router, tags=["Accounts"])
 app.include_router(trips.router, tags=["Trips"])
 app.include_router(trip_notes.router, tags=["Trip Note"])
+app.include_router(parks.router, tags=["Parks"])
 
 app.add_middleware(
     CORSMiddleware,
