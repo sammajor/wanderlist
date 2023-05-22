d5428ea9e2ddf98f991a1029eda97a34bf841488
Sam Major <sam.m.major@gmail.com>
2023-05-22 16:04:49 -0500

    changes

--- ghi/src/Components/Trips/TripList.js
+++ ghi/src/Components/Trips/TripList.js
@@ -1,4 +1,9 @@
+
+
 function TripList() {
+
+
+
   return (
     <div>
       <h1>My UpComing Trips</h1>
@@ -11,75 +16,24 @@ function TripList() {
           </tr>
         </thead>
         <tbody>
-          {/* #### FILTER APPOINTMENTS BY STATUS AND LOOP THROUGH #### */}
-          {appointments
-            .filter((appointment) => appointment.status === "confirmed")
-            .map((appointment) => {
-              // #### CHECK IF VIN IN LIST OF AUTOS ####
-              if (
-                automobiles.some(
-                  (automobile) => automobile.vin == appointment.vin
-                )
-              ) {
-                // #### IF MATCH GET OUT OF LIST ####
-                const getAuto = automobiles.filter(
-                  (automobile) => automobile.vin == appointment.vin
-                );
-                // #### SET VIP STATUS EITHER "YES" OR "NO" ####
-                if (getAuto[0].sold == true) {
-                  appointment["vip"] = "yes";
-                } else if (getAuto[0].sold === false) {
-                  appointment["vip"] = "no";
-                }
-                // #### IF NO MATCH RETURN VIP STATUS "NO" ####
-              } else if (
-                !automobiles.some(
-                  (automobile) => automobile.vin === appointment.vin
-                )
-              ) {
-                appointment["vip"] = "no";
-              }
+          {/* #### FILTER TRIPS BY STATUS AND LOOP THROUGH #### */}
+          {trips
+            .filter((trip) => trip.status === "upcoming")
               // #### PARSE DATE TO DISPLAY IN TABLE ####
-              let dateTime = parseISO(appointment.date_time);
+              let startDate = parseISO(trip.start_date);
+              let endDate = parseISO(trip.end_date);
 
               return (
                 <tr key={trip.id}>
                   <td>{trip.park}</td>
-                  <td>{trip.start_date}</td>
-                  <td>{trip.end_date}</td>
-                  <td>{format(dateTime, "MM/dd/yyyy")}</td>
-                  <td>{format(dateTime, "h:mm b")}</td>
-                  <td>
-                    {appointment.technician.first_name}{" "}
-                    {appointment.technician.last_name}
-                  </td>
-                  <td>{appointment.reason}</td>
-                  <td>
-                    <button
-                      onClick={handleCancelButton}
-                      id={appointment.id}
-                      type="button"
-                      className="btn btn-danger"
-                    >
-                      Cancel
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      onClick={handleFinishButton}
-                      id={appointment.id}
-                      type="button"
-                      className="btn btn-success"
-                    >
-                      Finish
-                    </button>
-                  </td>
+                  <td>{format(startDate, "MM/dd/yyyy")}</td>
+                  <td>{format(endDate, "h:mm b")}</td>
                 </tr>
-              );
-            })}
+            );
+            }
         </tbody>
       </table>
     </div>
-  );
+    )
 }
 export default TripList;
