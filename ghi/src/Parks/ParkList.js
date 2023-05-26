ac4169e84dc8797a9b7825b22a38e3a45bc364d2
Sam Major <sam.m.major@gmail.com>
2023-05-26 12:18:31 -0500

    Grid styling and pagination button styling added

--- ghi/src/Parks/ParkList.js
+++ ghi/src/Parks/ParkList.js
@@ -4,65 +4,69 @@ import { useSelector } from "react-redux";
 import SearchBar from "../Search";
 import Pagination from "./ParkPagination";
 
-
 const ParkList = () => {
   const { data, isLoading } = useGetAllParksQuery();
-    const searchCriteria = useSelector((state) => state.parkSearch.value);
-    console.log(data, isLoading)
-    const filteredParks = (data) => {
-      if (!searchCriteria) return data;
-      return data.filter(park => park.full_name.toLowerCase().includes(searchCriteria.toLowerCase()))
-    }
-    const [currentPage, setCurrentPage] = useState(1);
-    const [maxPageLimit, setMaxPageLimit] = useState(30);
-    const [minPageLimit, setMinPageLimit] = useState(0);
-    const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  const searchCriteria = useSelector((state) => state.parkSearch.value);
+  console.log(data, isLoading);
+  const filteredParks = (data) => {
+    if (!searchCriteria) return data;
+    return data.filter((park) =>
+      park.full_name.toLowerCase().includes(searchCriteria.toLowerCase())
+    );
+  };
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPageLimit, setMaxPageLimit] = useState(30);
+  const [minPageLimit, setMinPageLimit] = useState(0);
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
 
-    const onPageChange = (pageNumber) => {
-      setCurrentPage(pageNumber);
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const onPrevClick = () => {
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setMaxPageLimit(maxPageLimit - pageNumberLimit);
+      setMinPageLimit(minPageLimit - pageNumberLimit);
     }
-    const onPrevClick = () => {
-      if((currentPage-1) % pageNumberLimit === 0){
-        setMaxPageLimit(maxPageLimit - pageNumberLimit);
-        setMinPageLimit(minPageLimit - pageNumberLimit);
-      }
-      setCurrentPage(prev=> prev-1)
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const onNextClick = () => {
+    if (currentPage + 1 > maxPageLimit) {
+      setMaxPageLimit(maxPageLimit + pageNumberLimit);
+      setMinPageLimit(minPageLimit + pageNumberLimit);
     }
+    setCurrentPage((prev) => prev + 1);
+  };
+  // const paginationAttributes = {
+  //     currentPage,
+  //     maxPageLimit,
+  //     minPageLimit,
+  //     data: filteredParks().data,
 
-    const onNextClick = () => {
-      if(currentPage+1 > maxPageLimit){
-        setMaxPageLimit(maxPageLimit + pageNumberLimit);
-        setMinPageLimit(minPageLimit + pageNumberLimit);
-      }
-      setCurrentPage(prev=> prev+1)
-    }
-    // const paginationAttributes = {
-    //     currentPage,
-    //     maxPageLimit,
-    //     minPageLimit,
-    //     data: filteredParks().data,
+  //   }
 
-    //   }
-
-  if (isLoading) return<div>...loading</div>
+  if (isLoading) return <div>...loading</div>;
   return (
     <div className="container">
       <h1>National Parks</h1>
-      < SearchBar/>
+      <SearchBar />
       <div className="row" data-masonry='{"percentPosition": true }'>
-        {!isLoading ? <Pagination currentPage={currentPage}
-                              onPrevClick={onPrevClick}
-                              onNextClick={onNextClick}
-                              onPageChange={onPageChange}
-                              maxPageLimit={maxPageLimit}
-                              minPageLimit={minPageLimit}
-                              data={data}
-                              filteredParks={filteredParks}
-                              />
-            : <div> Loading... </div>
-            }
-        </div>
+        {!isLoading ? (
+          <Pagination
+            currentPage={currentPage}
+            onPrevClick={onPrevClick}
+            onNextClick={onNextClick}
+            onPageChange={onPageChange}
+            maxPageLimit={maxPageLimit}
+            minPageLimit={minPageLimit}
+            data={data}
+            filteredParks={filteredParks}
+          />
+        ) : (
+          <div> Loading... </div>
+        )}
       </div>
+    </div>
   );
-}
+};
 export default ParkList;
--- ghi/src/Parks/ParkPagination.js
+++ ghi/src/Parks/ParkPagination.js
@@ -1,85 +1,111 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import ParkCard from "./ParkCard";
+import "./park.css";
 
 const Pagination = (props) => {
-    const { currentPage, onPrevClick, onNextClick, onPageChange, maxPageLimit, minPageLimit, data, filteredParks } = props;
-    const [totalPages, setTotalPages] = useState(5);
-    const pages = []
-    const filtered =  filteredParks(data)
-    for (let i=1 ; i<=totalPages; i++){
-        pages.push(i);
+  const {
+    currentPage,
+    onPrevClick,
+    onNextClick,
+    onPageChange,
+    maxPageLimit,
+    minPageLimit,
+    data,
+    filteredParks,
+  } = props;
+  const [totalPages, setTotalPages] = useState(5);
+  const pages = [];
+  const filtered = filteredParks(data);
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+  const pageNumbers = pages.map((page) => {
+    if (page <= maxPageLimit && page > minPageLimit) {
+      return (
+        <li
+          key={page}
+          id={page}
+          onClick={onPageChange}
+          className={currentPage === page ? "page-item-active" : null}
+        >
+          <a class="page-link" href="#">
+            {page}
+          </a>
+        </li>
+      );
+    } else {
+      return null;
     }
-    const pageNumbers = pages.map(page => {
-        if(page <= maxPageLimit && page > minPageLimit){
-            return(
-                <li key={page} id={page} onClick={onPageChange}
-                className={currentPage===page ? 'active' : null}>
-                {page}
-                </li>
-            );
+  });
+  let pageIncrementEllipses = null;
 
-        }else{
-            return null;
-    }
-    });
-    let pageIncrementEllipses = null;
+  if (pages.length > maxPageLimit) {
+    pageIncrementEllipses = <li onClick={onNextClick}>&hellip;</li>;
+  }
 
-    if (pages.length > maxPageLimit){
-        pageIncrementEllipses = <li onClick={onNextClick}>&hellip;</li>
-    }
+  let pageDecrementEllipses = null;
+  if (minPageLimit >= 1) {
+    pageDecrementEllipses = <li onClick={onPrevClick}>&hellip;</li>;
+  }
 
-    let pageDecrementEllipses = null;
-    if (minPageLimit >=1){
-        pageDecrementEllipses = <li onClick={onPrevClick}>&hellip;</li>
-    }
+  // const renderData = () => {
+  //     {filtered.map(park => {
+  //         return (
+  //         <ul>
+  //             <ParkCard key={park.park_id}
+  //             image={park.park_image}
+  //             name={park.full_name}
+  //             city={park.city}
+  //             state={park.state}
+  //             description={park.description}/>
+  //         </ul>
+  //     )
+  //     })
+  //     }
+  console.log(data);
 
-    // const renderData = () => {
-    //     {filtered.map(park => {
-    //         return (
-    //         <ul>
-    //             <ParkCard key={park.park_id}
-    //             image={park.park_image}
-    //             name={park.full_name}
-    //             city={park.city}
-    //             state={park.state}
-    //             description={park.description}/>
-    //         </ul>
-    //     )
-    //     })
-    //     }
-    console.log(data)
-
-    return(
-        <div className="main">
-            <div className="mainData">
-                 {filtered.map(park => {
-            return (
-                <ParkCard key={park.park_id}
-                park_image={park.park_image}
-                full_name={park.full_name}
-                city={park.city}
-                state={park.state}
-                description={park.description}/>
-        )
-        })
-        }
-            </div>
-            <ul className="pageNumbers">
-               <li>
-                   <button onClick={onPrevClick} disabled={currentPage === pages[0]}>Prev</button>
-               </li>
-               {pageDecrementEllipses}
-                {pageNumbers}
-               {pageIncrementEllipses}
-                <li>
-                   <button onClick={onNextClick} disabled={currentPage === pages[pages.length-1]}>&gt;Next</button>
-               </li>
-            </ul>
-        </div>
-    )
-
+  return (
+    <>
+      {filtered.map((park) => {
+        return (
+          <ParkCard
+            key={park.park_id}
+            park_image={park.park_image}
+            full_name={park.full_name}
+            city={park.city}
+            state={park.state}
+            description={park.description}
+          />
+        );
+      })}
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li clasName="page-item">
+            <button
+              className="btn btn-outline-primary"
+              onClick={onPrevClick}
+              disabled={currentPage === pages[0]}
+            >
+              Prev
+            </button>
+          </li>
+          {pageDecrementEllipses}
+          {pageNumbers}
+          {pageIncrementEllipses}
+          <li clasName="page-item">
+            <button
+              className="btn btn-outline-primary"
+              onClick={onNextClick}
+              disabled={currentPage === pages[pages.length - 1]}
+            >
+              &gt;Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
-
 
 export default Pagination;
