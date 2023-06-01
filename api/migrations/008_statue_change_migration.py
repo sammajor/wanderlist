9d13e2690ac1aa53322405a1fefd912b994aea19
yin-Tung Chen <sandychen_710@hotmail.com>
2023-06-01 11:40:33 -0500

    add 008 migration table for trip status change

---
+++ api/migrations/008_statue_change_migration.py
@@ -0,0 +1,15 @@
+steps = [
+    [
+        #"UP" SQL statement
+        """
+        CREATE TYPE status AS ENUM ('Pending', 'Completed', 'Cancelled');
+        ALTER TABLE trips,
+        ADD COLUMN trip_status status NOT NULL DEFAULT 'Pending',
+
+        """,
+        # "DOWN" SQL statement
+        """
+
+        """
+    ]
+]
