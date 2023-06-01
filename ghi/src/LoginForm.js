5e1cb0f4b8efbdc64f70ce88bf4891f546b8f40a
yin-Tung Chen <sandychen_710@hotmail.com>
2023-06-01 11:10:52 -0500

    login form modify, makesure un-register user cannot login

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -1,55 +1,70 @@
 import { useState, useEffect } from "react";
-import { useLoginMutation } from "./store/apiSlice";
 import { useNavigate } from "react-router-dom";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 
-
+import { useLoginMutation } from "./store/apiSlice";
 
 const LoginForm = () => {
-    const [login] = useLoginMutation();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [login, { isLoading, isSuccess, error }] = useLoginMutation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login({ username, password });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        login({username, password})
-        navigate('/test')
+  useEffect(() => {
+    if (isSuccess === true) {
+      navigate("/test");
     }
-    return (
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-                <h1>Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="Login_username" className="form-label">Username</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="Login_username"
-                            value={username}
-                            onChange={(e) => {
-                                const { value } = e.target;
-                                setUsername(value)
-                            }}
-                            />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="Login_password" className="form-label">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="Login_password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-success">Submit</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+  }, [isSuccess]);
 
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h1>Login</h1>
+          {error ? (
+            <Alert variant="danger">
+              <Alert.Heading>Failed to login</Alert.Heading>
+              {error?.data?.detail || "An unknown error occured."}
+            </Alert>
+          ) : null}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="Login_username" className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="email"
+                required
+                placeholder="your@email.com"
+                value={username}
+                disabled={isLoading}
+                onChange={(e) => {
+                  const { value } = e.target;
+                  setUsername(value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group controlId="Login_password" className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                required
+                value={password}
+                disabled={isLoading}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button type="submit" variant="success" disabled={isLoading}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default LoginForm;
