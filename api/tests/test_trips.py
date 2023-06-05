cdb122a9e840523efe11d08300644551f7a4b00b
Drake Phamanivong <drakephamanivong1@gmail.com>
2023-06-05 23:07:10 +0000

    Create Trip Test

--- api/tests/test_trips.py
+++ api/tests/test_trips.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from main import app
+import json
 from queries.trips import TripQueries
+from models.trips import TripIn
 from authenticator import authenticator
 
 client = TestClient(app)
@@ -14,6 +16,15 @@ class FakeTripQueries():
             "end_date": "2023-06-07",
             "park": "Aspen",
             "trip_status": "Completed"
+        }
+    def create(self, account_id: int, trip: TripIn):
+        return {
+            "id": 1,
+            "account_id": 6,
+            "start_date": "2023-06-24",
+            "end_date": "2023-06-28",
+            "park": "Acadia National Park",
+            "trip_status": "pending"
         }
 
     def get_all_trips(self, account_id: int):
@@ -55,3 +66,35 @@ def test_get_all_trips():
     # Assert
     assert res.status_code == 200
     assert data == []
+
+
+def fake_get_current_account_data():
+    return {'id': 1}
+
+
+
+def test_create_trip():
+    #Arrange
+    app.dependency_overrides[TripQueries] = FakeTripQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+
+    trip_in = {
+        "start_date": "2023-06-24",
+        "end_date": "2023-06-28",
+        "park": "Acadia National Park"
+    }
+    res = client.post("/api/trips", json=trip_in)
+    data = res.json()
+
+    #Assert
+    assert res.status_code == 200
+    assert data == {
+        "id": 1, "account_id": 6,
+        "start_date": "2023-06-24",
+        "end_date": "2023-06-28",
+        "park":"Acadia National Park",
+        "trip_status": "pending"
+    }
+
+    #cleanup
+    app.dependency_overrides = {}
