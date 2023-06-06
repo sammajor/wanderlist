5604c758d0b963c9cbd4d84437966b098ea570e9
Sam Major <sam.m.major@gmail.com>
2023-06-06 14:32:43 -0500

    JS linting

--- ghi/src/Accounts/LogoutForm.js
+++ ghi/src/Accounts/LogoutForm.js
@@ -2,25 +2,26 @@ import { useLogoutMutation } from "../store/apiSlice";
 import { useNavigate } from "react-router";
 
 const LogoutForm = () => {
-    const[logout] = useLogoutMutation();
-    const navigate = useNavigate();
+  const [logout] = useLogoutMutation();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        logout()
-        navigate('/login')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    logout();
+    navigate("/login");
+  };
 
-return (
+  return (
     <div className="row">
-        <div className="col-md-6 offset-md-3">
-            <h1>Logout</h1>
-            <form onSubmit={handleSubmit}>
-                <button type="submit" className="btn btn-success">Logout</button>
-            </form>
-        </div>
+      <div className="col-md-6 offset-md-3">
+        <h1>Logout</h1>
+        <form onSubmit={handleSubmit}>
+          <button type="submit" className="btn btn-success">
+            Logout
+          </button>
+        </form>
+      </div>
     </div>
-
-);
-}
+  );
+};
 export default LogoutForm;
--- ghi/src/store/parkSearchSlice.js
+++ ghi/src/store/parkSearchSlice.js
@@ -1,22 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: null
+  value: null,
 };
 
 export const parkSearchSlice = createSlice({
-    name: 'parkSearch',
-    initialState,
-    reducers: {
-        reset: (state) => {
-            state.value = initialState.value;
-        },
-        search: (state, action) => {
-            state.value = action.payload;
-        }
-    }
-})
+  name: "parkSearch",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.value = initialState.value;
+    },
+    search: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
 
-export const { reset, search } = parkSearchSlice.actions
+export const { reset, search } = parkSearchSlice.actions;
 
-export default parkSearchSlice.reducer
+export default parkSearchSlice.reducer;
